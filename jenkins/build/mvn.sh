6da52d9f0a6d288a0ab90373ba3e0dcd86ffafe9
Jenkins <jenkins@jenkins.example.com>
2020-07-02 07:06:43 -0400

    first commmit

---
+++ jenkins/build/mvn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "***************************"
+echo "** Building jar ***********"
+echo "***************************"
+
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/docker-maven-pipeline
+#WORKSPACE=/home/jenkins/jenkins-data/pipeline
+
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
---
+++ jenkins/deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo maven-project > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
+
+scp  /tmp/.auth 192.168.4.142:/tmp/.auth
+scp  ./jenkins/deploy/publish 192.168.4.142:/tmp/publish
+ssh  192.168.4.142 "/tmp/publish"
