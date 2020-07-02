8e29b732cf0f34f3d63eee5868ecb31deaa29336
Jenkins <jenkins@jenkins.example.com>
2020-07-02 07:46:06 -0400

    changed message

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,4 +6,5 @@ echo $PASS >> /tmp/.auth
 
 scp  /tmp/.auth 192.168.4.142:/tmp/.auth
 scp  ./jenkins/deploy/publish 192.168.4.142:/tmp/publish
+scp  ./jenkins/deploy/h 192.168.4.142:/tmp/publish
 ssh  192.168.4.142 "/tmp/publish"
